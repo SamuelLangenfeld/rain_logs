d1d60a4c7661d85d0462f6112044b472c89e5343
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-07-20 13:11:19 -0400

    create embedded google map for sites show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+
+  def format_google_address
+  end
 end
--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,4 +1,6 @@
 
-
 module SitesHelper
+  def format_address_for_google_query(address)
+    address.gsub ' ', '+'
+  end
 end
