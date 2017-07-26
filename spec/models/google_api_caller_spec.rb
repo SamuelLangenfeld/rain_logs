1530c9857db014c038491af2fb0640b5fc209b73
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-07-25 22:49:30 -0400

    more rspec testing

--- spec/models/google_api_caller_spec.rb
+++ spec/models/google_api_caller_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe GoogleAPICaller, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "get_location_info" do
+    it "returns the correct latitude" do
+      puts (GoogleAPICaller.new.get_location_info("6369 lakewood dr falls church va")["address"])
+      expect(GoogleAPICaller.new.get_location_info("the white house")).to be true
+    end
+
+    it "returns the correct longitude" do
+    end
+  end
 end
