3b240db7dc725ea1af34a188d93d3d1c97eae26e
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-07-27 23:28:27 -0400

    specify error type in rspec test on site model

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Site, :type => :model  do
   siting=Site.new()
   siting.address="Eiffel Tower"
   it "raises error if outside US" do
-    expect{siting.get_site_attributes}.to raise_error
+    expect{siting.get_site_attributes}.to raise_error(StandardError)
   end
 end
