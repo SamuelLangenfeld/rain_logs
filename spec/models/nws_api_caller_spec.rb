06cd304c765cce56eedf08070f0b6240e80235fb
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-08-26 21:12:34 -0400

    modify tests/models because the callers are no longer persisted to database

--- spec/models/nws_api_caller_spec.rb
+++ spec/models/nws_api_caller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe NWSAPICaller, type: :model do
 
+  before(:all) do
+    @precipitation_data=NWSAPICaller.get_precipitation_data("KDCA")
+  end
+
   describe "get_station_id" do
     it "returns the correct observation station id for the lat/long given" do
       expect(NWSAPICaller.get_station_id("38.8977","-77.0365")).to eq("KDCA")
@@ -12,17 +16,15 @@ RSpec.describe NWSAPICaller, type: :model do
     
 
     it "returns a hash of precipitation data if given an observation station id" do
-      expect(NWSAPICaller.get_precipitation_data("KDCA")).to be_instance_of(Hash)
+      expect(@precipitation_data).to be_instance_of(Hash)
     end
 
     it "returns a hash with a day_pecip string" do
-      new_hash=NWSAPICaller.get_precipitation_data("KDCA")
-      expect(new_hash[:week_precip]).to be_instance_of(String)
+      expect(@precipitation_data[:week_precip]).to be_instance_of(String)
     end
 
     it "returns a hash with a day_pecip string" do
-      new_hash=NWSAPICaller.get_precipitation_data("KDCA")
-      expect(new_hash[:day_precip]).to be_instance_of(String)
+      expect(@precipitation_data[:day_precip]).to be_instance_of(String)
     end
   end
 
